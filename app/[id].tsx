9221b8486dd29cb9c71ab9522e9a5b6f4d06bdf2
indra.pyla <indra.pyla@infosys.com>
2024-09-15 12:59:30 +0530

    footer

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,8 +1,9 @@
-import {Text} from 'react-native';
+import {Text,View,Image, Pressable} from 'react-native';
 
 import events from '~/assets/events.json';
+import dayjs from 'dayjs';
+import { useLocalSearchParams,Stack } from 'expo-router';
 
-import { useLocalSearchParams } from 'expo-router';
 
 
 
@@ -10,8 +11,31 @@ export default function EventsPage() {
 
     const { id } = useLocalSearchParams();
     const event = events.find((event) => event.id === id);
+if(event==null){
+    return <Text>Event not found</Text>
+}
 
   return (
+<>
+<View className='flex-1 p-3 bg-white'>
+    <Stack.Screen options={{ title: 'Event' }} />
+    <View className='w-full'><Image source={{uri:event?.image}} 
+className='aspect-video  rounded-xl'></Image></View>
 
-    <Text>Events Page: {event?.title}</Text>)
+<Text className='text-2xl font-bold '>{event?.title}</Text>
+<Text className='text-md font-semibold uppercase text-amber-700'>{dayjs(event.datetime).format('ddd D,MMM · h:mm A')}</Text>
+<Text className='text-lg'>{event?.description}</Text>
+
+<View className=' flex-row border-t-2 border-gray-200 items-center justify-between absolute bottom-0 left-0 right-0 p-5'>
+<Text className='text-xl font-bold'>Free</Text>
+<Pressable>
+<Text className='text-xl bg-red-400 px-8 p-5 rounded-lg text-white font-bold'>Join and RSVP</Text>
+</Pressable>
+
+</View>
+
+    </View>
+
+</>
+  );
 }
